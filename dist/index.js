cf0e29baa903c546f1bd8cd00c33ef54bf43b69c
rovermaCT <verma.rohit233@gmail.com>
2021-04-16 11:03:29 +0530

    First Commit Files

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.hello = void 0;
+var world = 'World';
+function hello(word) {
+    if (word === void 0) { word = world; }
+    return 'Hello ${word}!';
+}
+exports.hello = hello;
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+const world = 'World';
+
+export function hello(word: string = world): string {
+    return 'Hello ${word}!';
+}
